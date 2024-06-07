8bd9f5012f5edef4dbe668e1862dc444ca9b4776
gigig12 <gigig@tutamail.com>
2024-06-07 18:33:06 +0300

    update translations

---
+++ Dynasty-Razer/DynastyRazer/Windows/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DynastyRazer.Services;
+using DynastyRazer.ViewModels;
+using System.Windows;
+
+namespace DynastyRazer.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IMangaProviderService _mangaProviderService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _mangaProviderService = new DynastyScansService();
+            DataContext = new MainViewModel(_mangaProviderService);
+        }
+
+        private void MenuItemAbout_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow window = new AboutWindow();
+            window.Show();
+        }
+
+        private void MenuItemSettings_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(DataContext is SettingsViewModel))
+                DataContext = new SettingsViewModel(_mangaProviderService);
+        }
+
+        private void MenuItemStarts_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(DataContext is MainViewModel))
+                DataContext = new MainViewModel(_mangaProviderService);
+        }
+    }
+}
