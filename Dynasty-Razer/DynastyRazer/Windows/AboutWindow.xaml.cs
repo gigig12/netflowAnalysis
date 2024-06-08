771ea78339e8e476e8d7961284314639d44d299f
gigig12 <gigig@tutamail.com>
2024-06-08 18:33:06 +0300

    fixed validation issues

---
+++ Dynasty-Razer/DynastyRazer/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace DynastyRazer.Windows
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LinkGithub_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/lolzK");
+        }
+    }
+}
