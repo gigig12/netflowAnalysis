67abd0d20d73b6563666a4116a0dec91528ccc64
gigig12 <gigig@tutamail.com>
2024-06-14 18:33:06 +0300

    Improve error handling in API

---
+++ Dynasty-Razer/DynastyRazer/Services/IMangaProviderService.cs
@@ -0,0 +1,29 @@
+﻿using DynastyRazer.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace DynastyRazer.Services
+{
+    public interface IMangaProviderService
+    {
+        Task<List<SerieListItem>> RetrieveAllSeries();
+
+        Task<SerieDetails> RetrieveSerieDetails(SerieListItem filter);
+
+        /// <summary>
+        /// Assign ChapterDetails to the ChapterListItem. Should be lazy loaded (on download start)
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        Task AssignChapterDetails(List<ChapterListItem> list);
+
+        Task StartDownload(List<ChapterListItem> chaptersToDownload);
+
+        event EventHandler<string> PageDownloadStateChanged;
+
+    }
+}
+
+
