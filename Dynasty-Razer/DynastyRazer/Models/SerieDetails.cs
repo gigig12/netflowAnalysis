e7fa1a02a462b837a2903b0cee2e2edacb322805
gigig12 <gigig@tutamail.com>
2024-06-16 18:33:06 +0300

    Fix login issue

---
+++ Dynasty-Razer/DynastyRazer/Models/SerieDetails.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DynastyRazer.Models
+{
+    public class SerieDetails
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+        public List<ChapterListItem> Chapters { get; set; }
+    }
+}
