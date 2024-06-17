346695ef99ef8ac68dc32acd275d034d9ec2f675
gigig12 <gigig@tutamail.com>
2024-06-17 18:33:06 +0300

    update user roles

---
+++ Dynasty-Razer/DynastyRazer/Models/ChapterListItem.cs
@@ -0,0 +1,24 @@
+﻿using DynastyRazer.ViewModels;
+
+namespace DynastyRazer.Models
+{
+    public class ChapterListItem : ViewModelBase
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public bool IsLocallySaved { get; set; }
+        public bool IsSelected { get; set; }
+        public bool IsChecked
+        {
+            get => IsLocallySaved || IsSelected;
+            set { IsSelected = value; NotifyPropertyChanged(); }
+        }
+        public string MangaTitle { get; set; }
+
+        /// <summary>
+        /// Lazy load property after a chapter is selected
+        /// </summary>
+        public ChapterDetails ChapterDetails { get; set; }
+
+    }
+}
