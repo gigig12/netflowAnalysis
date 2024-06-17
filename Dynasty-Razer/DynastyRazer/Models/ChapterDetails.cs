b3238deef72e7a814049e327e94ddf502385095a
gigig12 <gigig@tutamail.com>
2024-06-17 18:33:06 +0300

    improve database queries

---
+++ Dynasty-Razer/DynastyRazer/Models/ChapterDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DynastyRazer.Models
+{
+    public class ChapterDetails
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public List<ChapterPage> Pages { get; set; }
+    }
+}
+
