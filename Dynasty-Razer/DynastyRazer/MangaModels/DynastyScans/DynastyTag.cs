e7db294e7353a55c5f990980dea67093655314a8
gigig12 <gigig@tutamail.com>
2024-06-18 18:33:06 +0300

    fixed redirect issues

---
+++ Dynasty-Razer/DynastyRazer/MangaModels/DynastyScans/DynastyTag.cs
@@ -0,0 +1,9 @@
+﻿namespace DynastyRazer.MangaModels.DynastyScans
+{
+    public class DynastyTag
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Permalink { get; set; }
+    }
+}
