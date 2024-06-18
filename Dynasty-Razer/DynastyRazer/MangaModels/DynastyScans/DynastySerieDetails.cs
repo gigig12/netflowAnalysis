d56268d01a56d091b7a5df4b60fb2d7a6a087d4b
gigig12 <gigig@tutamail.com>
2024-06-18 18:33:06 +0300

    add new routes

---
+++ Dynasty-Razer/DynastyRazer/MangaModels/DynastyScans/DynastySerieDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DynastyRazer.MangaModels.DynastyScans
+{
+    public class DynastySerieDetails
+    {
+        public string Cover { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<DynastyChapter> Taggings { get; set; }
+    }
+}
