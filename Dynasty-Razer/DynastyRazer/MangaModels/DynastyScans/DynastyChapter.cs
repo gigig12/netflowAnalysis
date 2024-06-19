e49741d63040470415722e1c5567f856716217db
gigig12 <gigig@tutamail.com>
2024-06-19 18:33:06 +0300

    add new helper functions

---
+++ Dynasty-Razer/DynastyRazer/MangaModels/DynastyScans/DynastyChapter.cs
@@ -0,0 +1,13 @@
+﻿using DynastyRazer.MangaModels.DynastyScans;
+using System.Collections.Generic;
+
+namespace DynastyRazer.MangaModels.DynastyScans
+{
+    public class DynastyChapter
+    {
+        public string Title { get; set; }
+        public string Permalink { get; set; }
+        public List<DynastyPage> Pages { get; set; }
+        public List<DynastyTag> Tags { get; set; }
+    }
+}
