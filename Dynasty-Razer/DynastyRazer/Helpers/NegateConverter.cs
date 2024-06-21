bdfd76de2f917efd8c6c801193740f8cd08ca590
gigig12 <gigig@tutamail.com>
2024-06-21 18:33:06 +0300

    improve performance

---
+++ Dynasty-Razer/DynastyRazer/Helpers/NegateConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DynastyRazer.Helpers
+{
+    public class NegateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool ? !(bool)value : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool ? !(bool)value : value;
+        }
+    }
+}
