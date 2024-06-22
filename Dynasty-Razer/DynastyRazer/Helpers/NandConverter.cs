a442be5d1d83a22a5fde29389b08614cc00d8d38
gigig12 <gigig@tutamail.com>
2024-06-22 18:33:06 +0300

    add new util

---
+++ Dynasty-Razer/DynastyRazer/Helpers/NandConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace DynastyRazer.Helpers
+{
+    public class NandConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var relevantValues = new List<bool>();
+
+            foreach (var value in values)
+            {
+                if (value is bool)
+                    relevantValues.Add((bool)value);
+            }
+
+            return !relevantValues.Any(value => value);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
