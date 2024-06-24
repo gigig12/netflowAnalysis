9017776716fa3153facd23f58da8e2fad8192e7f
gigig12 <gigig@tutamail.com>
2024-06-24 18:33:06 +0300

    update user guide

---
+++ Dynasty-Razer/DynastyRazer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace DynastyRazer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SetLanguage("de-DE");
+            base.OnStartup(e);
+        }
+
+        public void SetLanguage(string langCode)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(langCode);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(langCode);
+        }
+    }
+}
